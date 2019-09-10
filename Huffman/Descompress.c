c02d521ee4630c328c1481dd9678ad7ac54306e7
Wilamis Aviz <46202593+WILAMISAVIZ@users.noreply.github.com>
2019-09-10 01:42:40 -0300

    change var names to english

--- Huffman/Descompress.c
+++ Huffman/Descompress.c
@@ -3,31 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-void get_header(FILE *file, unsigned int *tam_lixo, unsigned int *tam_arv) {
-	unsigned char bytes_cabecalho = (unsigned char) fgetc(file);
-	*(tam_lixo) = bytes_cabecalho >> 5;
+void get_header_descompress(FILE *file, unsigned int *len_trash, unsigned int *len_tree) {
+	unsigned char bytes_header = (unsigned char) fgetc(file);
+	*(len_trash) = bytes_header >> 5;
 
-	unsigned char five_bytes_from_tree = bytes_cabecalho << 3;
-	*(tam_arv) = five_bytes_from_tree >> 3;
-	bytes_cabecalho = (unsigned char) fgetc(file);
-	*(tam_arv) <<= 8;
-	*(tam_arv) |= bytes_cabecalho;
+	unsigned char five_bytes_from_tree = bytes_header << 3;
+	*(len_tree) = five_bytes_from_tree >> 3;
+	bytes_header = (unsigned char) fgetc(file);
+	*(len_tree) <<= 8;
+	*(len_tree) |= bytes_header;
 }
 
 Nodes *construct_tree_descompress(Nodes *Nodes_huff,
-		FILE *file_input_comprimido) {
+		FILE *file_input_compressed) {
 	unsigned char c;
-	fscanf(file_input_comprimido, "%c", &c);
+	fscanf(file_input_compressed, "%c", &c);
 
 	if (c == 42) {
 		Nodes_huff = CreatNode(0, c, NULL, NULL);
 		Nodes_huff->left = construct_tree_descompress(Nodes_huff->left,
-				file_input_comprimido);
+				file_input_compressed);
 		Nodes_huff->right = construct_tree_descompress(Nodes_huff->right,
-				file_input_comprimido);
+				file_input_compressed);
 	} else {
 		if (c == '\\') {
-			fscanf(file_input_comprimido, "%c", &c);
+			fscanf(file_input_compressed, "%c", &c);
 		}
 		Nodes_huff = CreatNode(0, c, NULL, NULL);
 	}
@@ -47,39 +47,38 @@ void descompress() {
 	Nodes *temp;
 	FILE *file_output, *file_input_descompress;
 	unsigned char character;
-	unsigned int tam_lixo, tam_arv;
-	long long int tam_file = 0, bytes = 0;
-	char arquivo_saida[100], nome_arquivo[30];
-	int tam_arquivo_entrada = 0, i;
+	unsigned int len_trash, len_tree;
+	long long int len_files = 0, bytes = 0;
+	char file_output_name[100], name_file[30];
+	int len_file_in = 0, i;
 	int *header;
 
 	printf("Nome do arquivo a descompactar?\n");
-	scanf("%s", nome_arquivo);
+	scanf("%s", name_file);
 
-	file_input_descompress = fopen(nome_arquivo, "rb");
+	file_input_descompress = fopen(name_file, "rb");
 
 	if (file_input_descompress == NULL) {
-		printf("Unable to open file: %s\n", nome_arquivo);
+		printf("Unable to open file: %s\n", name_file);
 		return;
 	}
 
 	bytes = FileSize(file_input_descompress);
 	fclose(file_input_descompress);
-	file_input_descompress = fopen(nome_arquivo, "rb");
+	file_input_descompress = fopen(name_file, "rb");
 
-	tam_arquivo_entrada = strlen(nome_arquivo);
+	len_file_in = strlen(name_file);
 
-	get_header(file_input_descompress, &tam_lixo, &tam_arv);
+	get_header_descompress(file_input_descompress, &len_trash, &len_tree);
 	Nodes *root = construct_tree_descompress(root, file_input_descompress);
 	Nodes *root_aux = root;
-	//printf("TAM FILE: %lld\n", bytes);
-	//printf("TAM LIXO: %d | TAM TREE: %d\n", tam_lixo, tam_arv);
-	for (i = 0; i < tam_arquivo_entrada - 5; i++) {
-		arquivo_saida[i] = nome_arquivo[i];
-	}
-	arquivo_saida[i] = '\0';
 
-	file_output = fopen(arquivo_saida, "wb");
+	for (i = 0; i < len_file_in - 5; i++) {
+		file_output_name[i] = name_file[i];
+	}
+	file_output_name[i] = '\0';
+
+	file_output = fopen(file_output_name, "wb");
 
 	temp = root;
 
@@ -92,19 +91,19 @@ void descompress() {
 				} else {
 					root_aux = root_aux->left;
 				}
-				if (eh_folha(root_aux)) {
+				if (is_leaf(root_aux)) {
 					fprintf(file_output, "%c", root_aux->character);
 					root_aux = root;
 				}
 			}
 		} else {
-			for (i = 7; i >= tam_lixo; i--) {
+			for (i = 7; i >= len_trash; i--) {
 				if (is_bit_set(character, i)) {
 					root_aux = root_aux->right;
 				} else {
 					root_aux = root_aux->left;
 				}
-				if (eh_folha(root_aux)) {
+				if (is_leaf(root_aux)) {
 					fprintf(file_output, "%c", root_aux->character);
 					root_aux = root;
 				}
